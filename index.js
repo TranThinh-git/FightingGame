1a44dcdfe3950acd6551ca11b799f3145235bd5d
TranThinh-git <thinh2000jp@gmail.com>
2023-03-01 23:42:33 +0900

    Basic control created

--- index.js
+++ index.js
@@ -6,6 +6,94 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = 0.2
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    change() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        if(this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } 
+        else this.velocity.y += gravity
+    }
+}
+const player1 = new Sprite({
+    position: {
+        x : 0,
+        y : 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const player2 = new Sprite({
+    position: {
+        x : 400,
+        y : 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+let lastKey
+
+const keys = {
+    a: {
+        pressed : false
+    },
+    d: {
+        pressed : false
+    }
+}
+
+
+
+window.addEventListener('keydown' , (event) => {
+    switch (event.key) {
+        case 'd' :
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+        case 'a' :
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 'w' :
+            player1.velocity.y = -10
+            break
+    }
+    // console.log(event.key)
+})
+
+window.addEventListener('keyup' , (event) => {
+    switch (event.key) {
+        case 'd' :
+            keys.d.pressed = false
+            break
+        case 'a' :
+            keys.a.pressed = false
+            break
+    }
+    // console.log(event.key)
+})
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
